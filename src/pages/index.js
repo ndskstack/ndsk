c7add50fb402b27c7552a8ded6f659c8fb3f3ff7
rockyshi <rockyshi1993@gmail.com>
2024-07-20 10:11:29 +0800

    nodestack

---
+++ src/pages/index.js
@@ -0,0 +1,18 @@
+
+const App = ({initialProps:{text,title}})=>{  
+  return (
+    <html>
+      <head>
+        <title>{title}</title> 
+      </head>
+      <body> 
+      <div>
+          {text}
+      </div>   
+      </body>
+    </html>
+  );
+
+}
+
+export default App;
---
+++ src/routes/index.js
@@ -0,0 +1,4 @@
+
+export default async (request,h)=>{
+    return h.render({title:"My app",text:"Hello world!"}); 
+}  
