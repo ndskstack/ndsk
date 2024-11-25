5f21b337a8ef6752193ca87d5c4bf829607d03f0
rockyshi <rockyshi1993@gmail.com>
2024-11-25 20:32:29 +0800

    update test

--- test.js
+++ test.js
@@ -1,13 +1,26 @@
 // import Child from 'child_process'
-const Child = require('node:child_process')
-// for(var i=0;i<=9;i++){
-    const time = new Date()
+const Child = require('node:child_process');
 
-    // const res = Child.spawn('node',['node_modules/@remix-run/dev/dist/cli.js','vite:dev']); //remix
-    // const res = Child.spawn('node',['node_modules/@ndsk/ndsk/index.js','dev']);   //umi
-    const res = Child.spawn('node',['node_modules/next/dist/bin/next',['build']]);   //nextjs
+const test = async (type)=>{
+    for(var i=0;i<10;i++){
+        const time = new Date()
     
-    res.stdout.on('finish', (data) => {
-        console.log(new Date()-time)
-    })
-// }
+        // const res = Child.spawn('node',['node_modules/@remix-run/dev/dist/cli.js',`vite:${type}`]); //remix
+        const res = Child.spawn('node',['node_modules/@ndsk/ndsk/index.js',type]);   //ndsk
+        // const res = Child.spawn('node',['node_modules/next/dist/bin/next',[type]]);   //nextjs
+        // const umi = ['node_modules/umi/bin/umi',type];  //or build
+        
+        const timer = await new Promise((resolve, reject) => {
+            res.stdout.on(type === 'dev' ? 'data' : 'finish', (data) => {    //data/finish
+                resolve(new Date()-time);
+            })
+        }).catch(error=>{
+            console.log(error.message)
+        });
+        console.log(timer)
+        process.exit();
+        
+    }
+}
+
+test('dev')
