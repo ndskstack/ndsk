82f08609f3961b102e8af8d433d94a592b463e29
rocky <rockyshi1993@gmail.com>
2024-12-02 13:44:38 +0800

    update test

--- test.js
+++ test.js
@@ -1,25 +1,25 @@
-// import Child from 'child_process'
+// import Child from 'child_process;'
 const Child = require('node:child_process');
 
-const test = async (type)=>{
-    for(var i=0;i<10;i++){
+const test = async (type) => {
+    for (var i = 0; i < 10; i++) {
         const time = new Date()
-    
+
         // const res = Child.spawn('node',['node_modules/@remix-run/dev/dist/cli.js',`vite:${type}`]); //remix
-        const res = Child.spawn('node',['node_modules/@ndsk/ndsk/index.js',type]);   //ndsk
+        const res = Child.spawn('node', ['node_modules/@ndsk/ndsk/index.js', type]);   //ndsk
         // const res = Child.spawn('node',['node_modules/next/dist/bin/next',[type]]);   //nextjs
         // const umi = ['node_modules/umi/bin/umi',type];  //or build
-        
+
         const timer = await new Promise((resolve, reject) => {
             res.stdout.on(type === 'dev' ? 'data' : 'finish', (data) => {    //data/finish
-                resolve(new Date()-time);
+                resolve(new Date() - time);
             })
-        }).catch(error=>{
+        }).catch(error => {
             console.log(error.message)
         });
         console.log(timer)
         process.exit();
-        
+
     }
 }
 
